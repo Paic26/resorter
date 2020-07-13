1db6610424e8e748c9d63a60c2d00dec396d6746
Paic26 <50775969+Paic26@users.noreply.github.com>
2020-07-13 03:52:53 +0100

    Add files via upload

---
+++ cogs/moderation.py
@@ -0,0 +1,103 @@
+import discord
+from discord.ext import commands
+import json
+
+def get_prefix(client, message):
+    with open('./json/prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+
+    return prefixes[str(message.guild.id)]
+
+bot = commands.Bot(command_prefix = get_prefix,  case_insensitive=True, owner_id=382947478422421516)
+
+class Moderation(commands.Cog):
+
+    def __init__(self, bot, *args, **kwargs):
+        self.bot = bot
+
+
+        #Events
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Moderation Cog is on')
+
+        #Commands
+
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def ban(self, ctx, member: discord.Member, *, reason=None):
+        await member.ban(reason=reason)
+        await ctx.send(f'Banned {member.mention}')
+        print(f'{member} was Banned from a server')
+
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def unban(self, ctx, *, member):
+        banned_users = await ctx.guild.bans()
+        member_name, member_discriminator = member.split('#')
+
+        for ban_entry in banned_users:
+            user = ban_entry.user
+
+            if (user.name, user.discriminator) == (member_name, member_discriminator):
+                await ctx.guild.unban(user)
+                await ctx.send(f'Unbanned {user.mention}')
+                print(f'{member} was Unbanned from a server')
+                return
+
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    async def mute(self, ctx, member: discord.Member = None):
+        role = discord.utils.get(ctx.guild.roles, name='Muted')
+        if not member:
+            await ctx.send('Please specify a member')
+            return
+        await member.add_roles(role)
+        await ctx.send('Muted')
+        print(f'{member} was Muted from a server')
+
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    async def unmute(self, ctx, member: discord.Member = None):
+        role = discord.utils.get(ctx.guild.roles, name='Muted')
+        if not member:
+            await ctx.send('Please specify a member')
+            return
+        await member.remove_roles(role)
+        await ctx.send('Unmuted')
+        print(f'{member} was Unmuted from a server')
+
+    @commands.command()
+    @commands.has_permissions(kick_members=True)
+    async def kick(self, ctx, member: discord.Member, *, reason=None):
+        await member.kick(reason=reason)
+        print(f'{member} was Kicked from a server')
+
+    @commands.command(aliases=['purge'])
+    @commands.has_permissions(manage_messages=True)
+    async def clear(self, ctx, amount=6):
+        await ctx.channel.purge(limit=amount)
+
+    @commands.command(aliases=['purgeall', 'purge_all'])
+    @commands.has_permissions(manage_messages=True)
+    async def clearall(self, ctx, amount=9999999999999):
+        await ctx.channel.purge(limit=amount)
+
+
+
+        #errors
+
+    @kick.error
+    async def kick_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send('Please select a user to kick.')
+
+    @ban.error
+    async def ban_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send('Please select a user to ban.')
+
+
+def setup(bot):
+    bot.add_cog(Moderation(bot))
+    print('Moderation Loaded')
---
+++ resorter.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+import  os
+
+
+bot = commands.Bot(command_prefix = ".",  case_insensitive=True, owner_id=382947478422421516)
+Bot = discord.client
+client = bot
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has logged in.\n-----------------------------')
+
+@bot.command()
+async def load(ctx, extension):
+    bot.load_extension(f'cogs.{extension}')
+
+@bot.command()
+async def unload(ctx, extension):
+    bot.unload_extension(f'cogs.{extension}')
+
+
+for filename in os.listdir('./cogs/'):
+    if filename.endswith('.py'):
+        client.load_extension((f'cogs.{filename[:-3]}'))
+
+
+@bot.command()
+async def hey(ctx):
+    await ctx.send('Welcome to the server!!')
+
+#bot.run(os.environ['TOKEN'])
+bot.run('NzMyMDUzNzkwNDc1NDE5Nzc4.XwvAXQ.Xl_jebTkXj-KFAIOM3fO1jRZMfY')
