0b13a1a2620195c664494e1f1b03a265b9b03940
Paic26 <50775969+Paic26@users.noreply.github.com>
2020-08-03 22:48:48 +0100

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 import json
-
+import random
 def get_prefix(client, message):
     with open('./json/prefixes.json', 'r') as f:
         prefixes = json.load(f)
