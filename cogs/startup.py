5ee6d4f8e83fdf2a8f937e852ebf9cb4186dd0c3
Paic26 <50775969+Paic26@users.noreply.github.com>
2020-08-14 23:55:40 +0100

    Update startup.py

--- cogs/startup.py
+++ cogs/startup.py
@@ -31,7 +31,7 @@ class Startup(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, member:discord.Member):
         channel = discord.utils.get(member.guild.channels, name='➤💙main-lobby')
-        role = discord.utils.get(member.guild.roles, name='Member')
+        role = discord.utils.get(member.guild.roles, name='✅Verified✅')
         memberCount = len(set(bot.get_all_members()))
         value = random.randint(0, 0xffffff)
         embed = discord.Embed(
