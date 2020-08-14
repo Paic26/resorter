ea08a3b69357576ce03be34e6a0bd69a64364591
Paic26 <50775969+Paic26@users.noreply.github.com>
2020-08-15 00:21:03 +0100

    Update startup.py

--- cogs/startup.py
+++ cogs/startup.py
@@ -46,7 +46,8 @@ class Startup(commands.Cog):
         print(f'{member} has joined a server')
 
     @commands.Cog.listener()
-    async def on_member_remove(self, ctx, member:discord.Member):
+    async def on_member_remove(self, member:discord.Member):
+        channel = discord.utils.get(member.guild.channels, name='➤💙main-lobby')
         memberCount = len(set(bot.get_all_members()))
         value = random.randint(0, 0xffffff)
         embed = discord.Embed(
@@ -57,7 +58,7 @@ class Startup(commands.Cog):
         embed.set_author(name=f" {member}", icon_url=member.avatar_url)
         embed.add_field(name=f'{member} has left the server', value="\u200b", inline=False)
         embed.set_footer(text=f"Members, {memberCount}")
-        await ctx.send(embed=embed)
+        await channel.send(embed=embed)
         print(f'{member} has left a server')
 
 def setup(bot):
